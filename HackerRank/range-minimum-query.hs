ac19f30a6aba275064c30d15de06d5f0e0df3688
FiveEyes <findthegateopen@gmail.com>
2015-03-09 03:14:59 -0700

    update indents

--- HackerRank/range-minimum-query.hs
+++ HackerRank/range-minimum-query.hs
@@ -26,23 +26,23 @@ updateSegTree :: (a -> a -> a) -> SegTree a -> Int -> Int -> a -> SegTree a
 updateSegTree _ Leaf _ _ _ = Leaf
 updateSegTree f (Node nl nr na lc rc) l r a = if l <= nl && nr <= r then Node nl nr a lc rc
   else if r <= m then let newlc = updateSegTree f lc l r a in Node nl nr (f (getItem newlc) (getItem rc)) newlc rc
-	else if l >  m then let newrc = updateSegTree f rc l r a in Node nl nr (f (getItem lc) (getItem newrc)) lc newrc
+  else if l >  m then let newrc = updateSegTree f rc l r a in Node nl nr (f (getItem lc) (getItem newrc)) lc newrc
   else
-	  let newlc = updateSegTree f lc l r a in
+    let newlc = updateSegTree f lc l r a in
     let newrc = updateSegTree f rc l r a in
-    Node nl nr (f (getItem newlc) (getItem newrc)) newlc newrc
-		where m = fst $ divMod (nl + nr) 2
+      Node nl nr (f (getItem newlc) (getItem newrc)) newlc newrc
+  where m = fst $ divMod (nl + nr) 2
 
 querySegTree :: (a -> a -> a) -> SegTree a -> Int -> Int -> a
 querySegTree f (Node nl nr na lc rc) l r =
   if l <= nl && nr <= r then na
   else if r <= m then querySegTree f lc l r
-	else if l >  m then querySegTree f rc l r
+  else if l >  m then querySegTree f rc l r
 	else
-	  let la = querySegTree f lc l r in
-	  let ra = querySegTree f rc l r in
-		f la ra
-		where m = fst $ divMod (nl + nr) 2
+    let la = querySegTree f lc l r in
+    let ra = querySegTree f rc l r in 
+      f la ra
+  where m = fst $ divMod (nl + nr) 2
 
 solve_seg :: SegTree Int -> [Int] -> Int
 solve_seg st [a, b] = querySegTree min st a b
