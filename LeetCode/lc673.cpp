b9d2c5c113941a7b909d065d2d303e6ad588651b
Han <hanzh.xu@gmail.com>
2019-06-04 03:56:23 -0700

    Create lc673.cpp

---
+++ LeetCode/lc673.cpp
@@ -0,0 +1,90 @@
+vector<int> s[2100];
+vector<int> ss[2100];
+int n, c[2100], l[2100];
+
+
+vector<int> longest;
+
+int bs(vector<int> &t, int x) {
+    int n = t.size();
+    int beg = 0, end = n, mid;
+    while(beg < end) {
+        mid = (beg + end) / 2;
+        if(t[mid] < x) {
+            beg = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+    return end;
+}
+
+void print(vector<int> &t) {
+    for(int i = 0; i < t.size(); ++i) {
+        cout << t[i] << ' ';
+    }
+    cout << endl;
+}
+
+int bs2(vector<int> &t, int x) {
+    int n = t.size();
+    int beg = 0, end = n, mid;
+    while(beg < end) {
+        mid = (beg + end) / 2;
+        if(t[mid] >= x) {
+            beg = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+    return end;
+}
+
+
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        n = nums.size();
+        longest.clear();
+        for(int i = 0; i <= n; ++i) {
+            s[i].clear();
+            ss[i].clear();
+            ss[i].push_back(0);
+        }
+        for(int i = 0; i < n; ++i) {
+            int tmp = bs(longest, nums[i]);
+            if(tmp >= longest.size()) {
+                longest.push_back(nums[i]);
+            } else {
+                if(longest[tmp] > nums[i]) {
+                    longest[tmp] = nums[i];
+                }
+            }
+            l[i] = tmp;
+            
+            
+            if(tmp == 0) {
+                c[i] = 1;
+            } else {
+                //print(s[tmp-1]);
+                //cout << "bs2 " << nums[i] << " " << s[tmp-1].size() - bs2(s[tmp-1], nums[i]) << endl;           
+                int ssr = bs2(s[tmp-1], nums[i]);
+                c[i] = ss[tmp-1][s[tmp-1].size()] - ss[tmp-1][ssr];
+            }
+                        
+            s[tmp].push_back(nums[i]);
+            ss[tmp].push_back(ss[tmp][ss[tmp].size()-1] + c[i]);
+
+        }
+        //print(longest);
+        int m = longest.size() - 1;
+        int ans = 0;
+        for(int i = 0; i < n; ++i) {
+            //cout << '(' << l[i] << ',' << c[i] << ')';
+            if(l[i] == m) {
+                ans += c[i];
+            }
+        }
+        return ans;
+    }
+};
